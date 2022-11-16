f53678eb3274a7d6452aad5bda5012ed838f5595
Abhay1609 <abhay2110004@akgec.ac.in>
2022-11-16 17:18:57 +0530

    verift

---
+++ account/utils.py
@@ -0,0 +1,6 @@
+from django.core.mail import EmailMessage, send_mail
+class Util:
+    @staticmethod
+    def send_email(data):
+        email=EmailMessage(subject=data['subject'],body=data['body'],to=[data['to']])
+        email.send()
