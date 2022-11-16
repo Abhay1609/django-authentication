cb751dee4c75c88503e588f6768f760932e2d199
Divyanshu <divyanshurana312@gmail.com>
2022-11-16 19:11:35 +0530

    change lifetime of token

--- Auth_API/settings.py
+++ Auth_API/settings.py
@@ -129,8 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
    
 
 
